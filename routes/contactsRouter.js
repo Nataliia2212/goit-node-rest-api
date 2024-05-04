3be866a0c91c108aa2e2758188f96421bb9d6868
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-05-04 21:22:38 +0300

    ok

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,26 +1,37 @@
 import express from "express";
-import {
-  getAllContacts,
-  getOneContact,
-  deleteContact,
-  createContact,
-  updateContact,
-} from "../controllers/contactsControllers.js";
+import contactsControllers from "../controllers/contactsControllers.js";
+
+import validateBody from "../decorators/validateBody.js";
 
 import isEmptyBody from "../middlewares/isEmptyBody.js";
 
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get("/", contactsControllers.getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", contactsControllers.getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", contactsControllers.deleteContact);
 
-contactsRouter.post("/", isEmptyBody, createContact);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(createContactSchema),
+  contactsControllers.createContact
+);
 
-contactsRouter.put("/:id", isEmptyBody, updateContact);
+contactsRouter.put(
+  "/:id",
+  isEmptyBody,
+  validateBody(updateContactSchema),
+  contactsControllers.updateContact
+);
 
-contactsRouter.patch("/:id", isEmptyBody, updateContact);
+contactsRouter.patch("/:id", isEmptyBody, contactsControllers.updateContact);
 
 export default contactsRouter;
